a954721ac42c947230d914302e12efb586f81992
JR Stanley <jr@jrstn.ly>
2025-08-16 22:37:13 -0500

    Finalized single media page ui

--- templates/single-media-content.php
+++ templates/single-media-content.php
@@ -7,9 +7,20 @@
 
 $title = esc_html($media_item['title'] ?? '');
 $airDate = esc_html($plugin->format_date($media_item['airDate'] ?? ''));
-$duration = esc_html($plugin->format_duration($media_item['duration'] ?? 0));
-$categories = !empty($media_item['categories']) ? '<span class="media-categories">' . esc_html(implode(', ', (array)$media_item['categories'])) . '</span>' : '';
-$descriptionText = $media_item['searchText'] ?? '';
+$series = !empty($media_item['series']) ? '<span class="media-series"><strong>' . esc_html__('Series:', 'sardius-feed') . '</strong> ' . esc_html($media_item['series']) . '</span>' : '';
+
+// Create individual pills for each scripture reference
+$scripture = '';
+if (!empty($media_item['metadata']['bibleReference'])) {
+    $scripture_references = (array)$media_item['metadata']['bibleReference'];
+    $scripture_pills = array();
+    foreach ($scripture_references as $reference) {
+        $scripture_pills[] = '<span class="media-scripture">' . esc_html($reference) . '</span>';
+    }
+    $scripture = '<div class="media-scripture-container"><strong>' . esc_html__('Scripture:', 'sardius-feed') . '</strong><div class="scripture-pills">' . implode(' ', $scripture_pills) . '</div></div>';
+}
+
+$descriptionText = $media_item['description'] ?? '';
 $description = $descriptionText ? ('<div class="media-description"><h3>' . esc_html__('Description', 'sardius-feed') . '</h3><p>' . esc_html($descriptionText) . '</p></div>') : '';
 $video = $plugin->build_video_player_html($media_item);
 ?>
@@ -25,12 +36,10 @@ $video = $plugin->build_video_player_html($media_item);
                 <h1 class="media-title"><?php echo $title; ?></h1>
                 <div class="media-meta">
                     <span class="media-date">
-                        <strong><?php _e('Air Date:', 'sardius-feed'); ?></strong> <?php echo $airDate; ?>
+                        <strong><?php _e('Date:', 'sardius-feed'); ?></strong> <?php echo $airDate; ?>
                     </span>
-                    <span class="media-duration">
-                        <strong><?php _e('Duration:', 'sardius-feed'); ?></strong> <?php echo $duration; ?>
-                    </span>
-                    <?php echo $categories; ?>
+                    <?php echo $series; ?>
+                    <?php echo $scripture; ?>
                 </div>
             </div>
             
